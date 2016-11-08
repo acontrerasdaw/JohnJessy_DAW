cb91d859ef7b4e9b8adcd4d663f8eabbbf37e0aa
jaumealonso21 <jaumealonso@hotmail.com>
2016-11-08 16:46:13 +0100

    Version 0.5 Treball John

--- src/exe2_m8/Exe2_m8.java
+++ src/exe2_m8/Exe2_m8.java
@@ -17,6 +17,9 @@ public class Exe2_m8 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("EXE2 M8");
+        System.out.println("EXE2 M811111111");
+        System.out.println("EXE2 M822222222");
+        System.out.println("EXE2 M833333333");
     }
     
 }
