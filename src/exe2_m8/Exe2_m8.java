2c975683b7959bda53581f6ec5ef61544e7e5621
daw <daw@PCDAW>
2016-11-08 16:30:12 +0100

    berziom rama huno

---
+++ src/exe2_m8/Exe2_m8.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exe2_m8;
+
+/**
+ *
+ * @author daw
+ */
+public class Exe2_m8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("EXE2 M8");
+    }
+    
+}
